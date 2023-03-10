05f352f990da800578dd0b0aa380408c1e5fc223
TheCyberOne <joshua.knaven@gmail.com>
2023-03-10 13:12:24 +0100

    Added slicing to ingredients

--- Assets/_Le Fish/Scripts/Ingredient.cs
+++ Assets/_Le Fish/Scripts/Ingredient.cs
@@ -1,53 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
+[Serializable]
 public class Ingredient : MonoBehaviour
 {
-    [SerializeField] private MeshFilter meshFilter;
-    [SerializeField] private IngredientState currentState;
-    [SerializeField] private List<IngredientData> ingredientData;
+    public IngredientState CurrentState { get; private set; }
+    public int CurrentSlice { get; private set; }
 
-    private Dictionary<IngredientState, Mesh> _ingredients;
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] List<IngredientData> ingredients = new();
 
     [Serializable]
-    private class IngredientData
+    public class IngredientData
     {
-        [SerializeField] public IngredientState state;
-        [SerializeField] public Mesh mesh;
-    }
-
-    private void OnValidate()
-    {
-        var possibleEnums = Enum.GetValues(typeof(IngredientState)).ConvertTo<List<IngredientState>>();
-        int maxLength = possibleEnums.Count;
-        if (ingredientData.Count > maxLength)
-        {
-            ingredientData = ingredientData.Take(maxLength).ToList();
-            LogEnumWarning();
-        }
-        else
-        {
-            for (int i = 0; i < ingredientData.Count; i++)
-            {
-                if (possibleEnums.Contains(ingredientData[i].state))
-                {
-                    possibleEnums.Remove(ingredientData[i].state);
-                }
-                else
-                {
-                    LogEnumWarning();
-                    ingredientData[i] = new IngredientData { state = possibleEnums[0] };
-                    possibleEnums.RemoveAt(0);
-                }
-            }
-        }
-
-        static void LogEnumWarning() => Debug.LogWarning("There are no more unique states");
-
-        _ingredients = ingredientData.ToDictionary(ingredientPair => ingredientPair.state, ingredientPair => ingredientPair.mesh);
+        [SerializeField] public IngredientState[] states;
+        [SerializeField] public Mesh[] meshes;
     }
 
     public enum IngredientState
@@ -57,9 +26,70 @@ public class Ingredient : MonoBehaviour
         Burnt
     }
 
-    public void UpdateState(IngredientState state)
+    public void NextState(IngredientState state)
     {
-        currentState = state;
-        meshFilter.mesh = _ingredients[state];
+        CurrentState = state;
+        meshFilter.mesh = ingredients[CurrentSlice].meshes[(int)state];
+    }
+
+    public void Slice()
+    {
+        if (CurrentSlice < ingredients.Count - 1)
+            CurrentSlice++;
     }
 }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(Ingredient.IngredientData))]
+public class IngredientDataDrawer : PropertyDrawer
+{
+    const float PROPERTY_HEIGHT = 18;
+    const float PROPERTY_SPACING = 2;
+    const float TOTAL_PROPERTY_HEIGHT = PROPERTY_HEIGHT + PROPERTY_SPACING;
+
+    readonly Ingredient.IngredientState[] _ingredientStates = (Ingredient.IngredientState[])Enum.GetValues(typeof(Ingredient.IngredientState));
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
+        TOTAL_PROPERTY_HEIGHT * _ingredientStates.Length + TOTAL_PROPERTY_HEIGHT;
+
+    private void Setup(SerializedProperty property)
+    {
+        SerializedProperty meshesField = property.FindPropertyRelative(nameof(Ingredient.IngredientData.meshes));
+        SerializedProperty statesField = property.FindPropertyRelative(nameof(Ingredient.IngredientData.states));
+        for (int i = 0; i < _ingredientStates.Length; i++)
+        {
+            meshesField.InsertArrayElementAtIndex(i);
+            statesField.InsertArrayElementAtIndex(i);
+            statesField.GetArrayElementAtIndex(i).enumValueIndex = i;
+        }
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.FindPropertyRelative(nameof(Ingredient.IngredientData.meshes)).arraySize < _ingredientStates.Length)
+            Setup(property);
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        var labelPosition = new Rect(position.x, position.y, position.width, PROPERTY_HEIGHT);
+        var labelStyle = new GUIStyle { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold };
+        labelStyle.normal.textColor = Color.white;
+        EditorGUI.LabelField(labelPosition, label.text.Replace("Element", "Slice"), labelStyle);
+
+        SerializedProperty meshesField = property.FindPropertyRelative(nameof(Ingredient.IngredientData.meshes));
+
+        for (int i = 0; i < _ingredientStates.Length; i++)
+        {
+            var ingredientPosition = new Rect(position.x, position.y + (1 + i) * TOTAL_PROPERTY_HEIGHT, position.width, PROPERTY_HEIGHT);
+            meshesField.GetArrayElementAtIndex(i).objectReferenceValue = EditorGUI.ObjectField(
+                position: ingredientPosition,
+                label: new GUIContent(_ingredientStates[i].ToString()),
+                obj: meshesField.GetArrayElementAtIndex(i).objectReferenceValue,
+                objType: typeof(Mesh),
+                allowSceneObjects: false);
+        }
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
