5cccc36f3c97e72378d70c9ae058fbc14c87b2c6
DJHabibi <dariovanderlaangx@gmail.com>
2023-06-07 16:12:09 +0200

    Merge branch 'main' into Dario + 1/2 of falling object mechanics

---
+++ Assets/FallenObjectDetector.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FallenObjectDetector : MonoBehaviour
+{
+    public GameObject conchyHelper;
+    private HashSet<Collider> enteredObjects = new HashSet<Collider>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ingredient"))
+        {
+            if (!enteredObjects.Contains(other))
+            {
+                // Get the position of the entering object
+                Vector3 enteringObjectPosition = other.transform.position;
+
+                // Calculate the position for the conchyHelper below the entering object
+                Vector3 conchyPosition = enteringObjectPosition - new Vector3(0, 0.2f, 0);
+
+                // Instantiate the conchyHelper at the calculated position
+                GameObject conchyInstance = Instantiate(conchyHelper, conchyPosition, Quaternion.identity);
+
+                // Set the entering object as the child of the conchy instance
+                other.transform.SetParent(conchyInstance.transform);
+
+                // Make the entering object's Rigidbody kinematic and zero out the y-velocity
+                Rigidbody enteringObjectRigidbody = other.GetComponent<Rigidbody>();
+                if (enteringObjectRigidbody != null)
+                {
+                    enteringObjectRigidbody.useGravity = false;
+                    Vector3 velocity = enteringObjectRigidbody.velocity;
+                    velocity.y = 0f;
+                    enteringObjectRigidbody.velocity = velocity;
+                }
+
+                // Add the object to the set of entered objects
+                enteredObjects.Add(other);
+            }
+        }
+    }
+}
